80dbb96c3ef5445d79b69a9fa7ffbca469987592
Maël <mal.gouyet@epitech.eu>
2023-06-07 15:33:51 +0200

    ADD: add the main of the IA

--- IA/IA.cpp
+++ IA/IA.cpp
@@ -17,12 +17,46 @@ IA::~IA()
 {
 }
 
+void printUsage() {
+    std::cout << "USAGE: ./zappy_ai -p port -n name -h machine\n"
+              << "port is the port number\n"
+              << "name is the name of the team\n"
+              << "machine is the name of the machine; localhost by default\n";
+}
 
-// int main(int ac, char **av)
-// {
-//     if (ac != 4)
-//         return 84;
-//     IA IA(atoi(av[1]), av[2], av[3]);
-//     IA.communicateWithServer();
-//     return 0;
-// }
+
+int main(int ac, char **av)
+{
+    size_t port = 0;
+    std::string name;
+    std::string machine;
+    if (ac != 7 || std::string(av[1]) == "-help") {
+        printUsage();
+        return 84;
+    }
+    for (int i = 1; i < ac; i += 2) {
+        std::string flag = av[i];
+        if (flag.size() != 2 || flag[0] != '-') {
+            printUsage();
+            return 84;
+        }
+        switch (flag[1]) {
+            case ('p'):
+                port = std::atoi(av[i + 1]);
+                break;
+            case ('n'):
+                name = av[i + 1];
+                break;
+            case ('h'):
+                machine = av[i + 1];
+                break;
+            default:
+                std::cout << "Invalid argument: " << flag << std::endl;
+                printUsage();
+                return 84;
+        }
+    }
+    IA IA(port, name, machine);
+    IA.communicateWithServer();
+    return 0;
+}
