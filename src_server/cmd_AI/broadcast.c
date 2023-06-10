d58b574382ebc0d800bb37d3408352639d0009ef
JV <jean-vincent.amalfitano@epitech.eu>
2023-06-10 10:46:21 -0400

    MODID: add feat of Broacast just neet to add the  sound emission to the message

--- src_server/cmd_AI/broadcast.c
+++ src_server/cmd_AI/broadcast.c
@@ -7,17 +7,57 @@
 
 #include "server.h"
 
-void broadcast(__attribute__((unused)) client_manager_t *c,
-__attribute__((unused)) int nbClient,
-__attribute__((unused)) char *buff)
+size_t len_txt(char *buff)
 {
-    int len = (strlen(buff) - strlen(BRDTXT) + 1);
-    char *tmp = malloc(sizeof(char) * len);
-    size_t index = 0;
-    memset(tmp, 0, len);
-    for (size_t i = (strlen(BRDTXT) + 1); i < (strlen(buff) - 1); i++) {
-        tmp[index++] = buff[i];
+    size_t i = (strlen(BRDTXT) + 1);
+    size_t res = 0;
+
+    while (buff[i] != '\n' && buff[i] != '\0') {
+        i++;
+        res++;
     }
-    printf("len = %d\n", len);
-    printf("tmp = |%s|\n", tmp);
+    return res;
+}
+
+char *tile(client_manager_t *c, int nbClient)
+{
+    //TODO find client dir sound
+}
+
+char *do_txt(client_manager_t *c, int nbClient, char *message)
+{
+    size_t len = strlen("message ") + 1 + strlen(", ") + strlen(message);
+    char *txt = malloc(sizeof(char) * (len + 1));
+
+    memset(txt, 0, (len + 1));
+    strcat(txt, "message ");
+    strcat(txt, tile(c, nbClient));
+    strcat(txt, ", ");
+    strcat(txt, message);
+    return txt;
+}
+
+void send_all(client_manager_t *c, int nbClient, char *buff)
+{
+    char *txt = do_txt(c, nbClient, buff);
+
+    for (size_t i = 0; i < (c->nb_clients * c->nb_teams); i++) {
+        if (i != nbClient)
+            write(c->client_infos[i].client_socket, txt, strlen(txt));
+    }   
+}
+
+void broadcast(__attribute__((unused)) client_manager_t *c,
+__attribute__((unused)) int nbClient, char *buff)
+{
+    int len = len_txt(buff);
+    char *tmp = malloc(sizeof(char) * (len + 1));
+    size_t index = 0;
+
+    memset(tmp, 0, (len + 1));
+    for (size_t i = (strlen(BRDTXT) + 1); i < (strlen(buff) - 1); i++) {
+        tmp[index] = buff[i];
+        index++;
+    }
+    send_all(c, nbClient, buff);
 }
