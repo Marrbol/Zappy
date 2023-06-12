7670af76abefcbb503a450ed7bc32438608aecb1
JV <jean-vincent.amalfitano@epitech.eu>
2023-06-12 05:35:23 -0400

    FIX: fixing norme fault by creating a fucntion to get less lines to a the function invent

--- src_server/cmd_AI/invent.c
+++ src_server/cmd_AI/invent.c
@@ -7,6 +7,17 @@
 
 #include "server.h"
 
+void write_to(client_manager_t *c, int nbClient, size_t i)
+{
+    write(c->client_infos[nbClient].client_socket, ", ", 2);
+    write(c->client_infos[nbClient].client_socket, allInv[i].name,
+    strlen(allInv[i].name));
+    write(c->client_infos[nbClient].client_socket, " ", 1);
+    write(c->client_infos[nbClient].client_socket,
+    my_atoi(c->client_infos[nbClient].inv[i]),
+    strlen(my_atoi(c->client_infos[nbClient].inv[i])));
+}
+
 void invent(client_manager_t *c, int nbClient,
 __attribute__((unused)) char *buff)
 {
@@ -21,14 +32,7 @@ __attribute__((unused)) char *buff)
     strlen(my_atoi(c->client_infos[nbClient].inv[i])));
     i++;
     while (i < (LENINV - 1)) {
-        write(c->client_infos[nbClient].client_socket, ", ", 2);
-        write(c->client_infos[nbClient].client_socket, allInv[i].name,
-        strlen(allInv[i].name));
-        write(c->client_infos[nbClient].client_socket, " ", 1);
-        write(c->client_infos[nbClient].client_socket,
-        my_atoi(c->client_infos[nbClient].inv[i]),
-        strlen(my_atoi(c->client_infos[nbClient].inv[i])));
-
+        write_to(c, nbClient, i);
         i++;
     }
     write(c->client_infos[nbClient].client_socket, " ]\n", 3);
