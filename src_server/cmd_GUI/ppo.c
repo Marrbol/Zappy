efc32530f57ba0ecb098d7a9d5bcbea5bfe2988b
JV <jean-vincent.amalfitano@epitech.eu>
2023-06-21 05:20:57 -0400

    MODIF: add pnw command + execution

--- src_server/cmd_GUI/ppo.c
+++ src_server/cmd_GUI/ppo.c
@@ -10,5 +10,11 @@
 void ppo(__attribute__((unused)) client_manager_t *c,
 __attribute__((unused)) int nbClient,
 __attribute__((unused)) char *buff) {
-
+    //char *base = "ppo ";
+    //char *message = strdup(strtok(buff, base), "\n");
+    //int id = my_atoi(message);
+//
+    //printf("message = %s\n", message);
+    //printf("convert = %d\n", id);
+    
 }
